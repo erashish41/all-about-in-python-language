4bbeb5905918265c6574cc7cad1586732f468d24
Ashish Bhardwaj <erashish41@gmail.com>
2026-01-16 16:57:44 +0530

    Encapsulation in OOPs

--- oops.py
+++ oops.py
@@ -1,12 +1,12 @@
-class Car:
-    def __init__(self,brand,model):
-        self.brand = brand
-        self.model = model
+# class Car:
+#     def __init__(self,brand,model):
+#         self.brand = brand
+#         self.model = model
         
-        # init is also called constructor
+#         # init is also called constructor
         
-    def full_name(self):
-        return f"{self.brand} - {self.model}"
+#     def full_name(self):
+#         return f"{self.brand} - {self.model}"
         
 # # my_car = Car("Toyota", "Corolla")
 # # print(my_car.brand)
@@ -20,37 +20,37 @@ class Car:
 # # print(my_new_car.full_name())
 # # print("-----")
 
-# a. Inheritance
-class ElectricCar(Car):
-    def __init__(self,brand,model,battery_size):
-        super().__init__(brand,model)
-        self.battery_size = battery_size
-        
-my_tesla = ElectricCar("Tesla","Model S","88 kWh")
-print(my_tesla.brand)
-print(my_tesla.model)
-print(my_tesla.battery_size)
-print(my_tesla.full_name())
-
-
-# # b. Encapsulation
-# class Car:
-#     def __init__(self,brand,model):
-#         self.__brand = brand
-#         self.model = model
-        
-#     def full_name(self):
-#         return f"{self.__brand} - {self.model}"
-    
-#     def get_brand(self):
-#         return self.__brand + " !"
+# # a. Inheritance
 # class ElectricCar(Car):
 #     def __init__(self,brand,model,battery_size):
 #         super().__init__(brand,model)
-#         self.batter_size = battery_size
+#         self.battery_size = battery_size
         
-# my_electriccar = ElectricCar("Tesla", "Model S", "80kWh")
-# print(my_electriccar.get_brand())
+# my_tesla = ElectricCar("Tesla","Model S","88 kWh")
+# print(my_tesla.brand)
+# print(my_tesla.model)
+# print(my_tesla.battery_size)
+# print(my_tesla.full_name())
+
+
+# b. Encapsulation
+class Car:
+    def __init__(self,brand,model):
+        self.__brand = brand
+        self.model = model
+        
+    def full_name(self):
+        return f"{self.__brand} - {self.model}"
+    
+    def get_brand(self):
+        return self.__brand + " !"
+class ElectricCar(Car):
+    def __init__(self,brand,model,battery_size):
+        super().__init__(brand,model)
+        self.batter_size = battery_size
+        
+my_electriccar = ElectricCar("Tesla", "Model S", "80kWh")
+print(my_electriccar.get_brand())
 
 
 # # c. Polymorphism
