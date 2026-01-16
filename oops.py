a566f7b64463faa7194326630b48ff8a6e543d3b
Ashish Bhardwaj <erashish41@gmail.com>
2026-01-16 16:59:05 +0530

    @staticmethod in  OOPs

--- oops.py
+++ oops.py
@@ -53,49 +53,14 @@
 # print(my_electriccar.get_brand())
 
 
-# c. Polymorphism
-class Car:
-    total_car = 0
-    
-    def __init__(self,brand,model):
-        self.__brand = brand
-        self.model = model
-        Car.total_car += 1
-        
-    def full_name(self):
-        return f"{self.__brand} - {self.model}"
-    
-    def get_brand(self):
-        return self.__brand + " !"
-    
-    def fuel_type(self):
-        return ("Petrol or Diesel")
-    
-class ElectricCar(Car):
-    def __init__(self,brand,model,battery):
-        super().__init__(brand,model)
-        self.battery = battery
-        
-    def fuel_type(self):
-        return ("Electrical Charger")
-        
-my_electricCar = ElectricCar("Tesla","Model S", "80kWh")
-print(my_electricCar.get_brand())
-print(my_electricCar.full_name())
-print(my_electricCar.fuel_type())
-print(Car.total_car)
-print("----")
-
-my_car = Car("Maruti","Baleno")
-print(my_car.model)
-print(my_car.fuel_type())
-print(Car.total_car)
-
-# # d. with general method with @staticmethod
+# # c. Polymorphism
 # class Car:
+#     total_car = 0
+    
 #     def __init__(self,brand,model):
 #         self.__brand = brand
 #         self.model = model
+#         Car.total_car += 1
         
 #     def full_name(self):
 #         return f"{self.__brand} - {self.model}"
@@ -104,36 +69,71 @@ print(Car.total_car)
 #         return self.__brand + " !"
     
 #     def fuel_type(self):
-#         return "Petrol or Diesel"
+#         return ("Petrol or Diesel")
     
-#     @staticmethod
-#     def general_description():
-#         return "Car is mode of transport"
 # class ElectricCar(Car):
-#     def __init__(self, brand, model,battery):
+#     def __init__(self,brand,model,battery):
 #         super().__init__(brand,model)
 #         self.battery = battery
         
 #     def fuel_type(self):
-#         return "Electric Charge"
+#         return ("Electrical Charger")
+        
+# my_electricCar = ElectricCar("Tesla","Model S", "80kWh")
+# print(my_electricCar.get_brand())
+# print(my_electricCar.full_name())
+# print(my_electricCar.fuel_type())
+# print(Car.total_car)
+# print("----")
+
+# my_car = Car("Maruti","Baleno")
+# print(my_car.model)
+# print(my_car.fuel_type())
+# print(Car.total_car)
+
+# d. with general method with @staticmethod
+class Car:
+    def __init__(self,brand,model):
+        self.__brand = brand
+        self.model = model
+        
+    def full_name(self):
+        return f"{self.__brand} - {self.model}"
+    
+    def get_brand(self):
+        return self.__brand + " !"
+    
+    def fuel_type(self):
+        return "Petrol or Diesel"
+    
+    @staticmethod
+    def general_description():
+        return "Car is mode of transport"
+class ElectricCar(Car):
+    def __init__(self, brand, model,battery):
+        super().__init__(brand,model)
+        self.battery = battery
+        
+    def fuel_type(self):
+        return "Electric Charge"
         
     
-# my_car = Car("Toyota", "Corolla")
-# print(my_car.model)
-# print(my_car.get_brand())
-# print(my_car.full_name())
-# print(my_car.fuel_type())
-# print(my_car.general_description())
-# print("-------")
+my_car = Car("Toyota", "Corolla")
+print(my_car.model)
+print(my_car.get_brand())
+print(my_car.full_name())
+print(my_car.fuel_type())
+print(my_car.general_description())
+print("-------")
 
 
-# my_electriccar = ElectricCar("Tesla","Model s","80kWh")
-# print(my_electriccar.model)
-# print(my_electriccar.get_brand())
-# print(my_electriccar.battery)
-# print(my_electriccar.full_name())
-# print(my_electriccar.fuel_type())
-# print(my_electriccar.general_description())
+my_electriccar = ElectricCar("Tesla","Model s","80kWh")
+print(my_electriccar.model)
+print(my_electriccar.get_brand())
+print(my_electriccar.battery)
+print(my_electriccar.full_name())
+print(my_electriccar.fuel_type())
+print(my_electriccar.general_description())
 
 
 # # e. with general method with @property
